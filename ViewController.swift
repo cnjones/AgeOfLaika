ab5fe9823a2449639aa7b72e09ec47b98d48c922
cnjones <inrianian@mac.com>
2014-09-29 21:52:23 -0400

    Version 2
    Major overhaul of the “commit” button:
    - allowed entry of doubles
    - prevented crash after enter a letter into the “Human Years” text box
    - rewrote formula to multiply first two years by 10.5, and each year
    thereafter (if any) by 4
    - squashed multiple crash points and bugs

--- ViewController.swift
+++ ViewController.swift
@@ -24,9 +24,43 @@ class ViewController: UIViewController {
     }
 
     @IBAction func convertButtonPressed(sender: UIButton) {
-        dogYearsLabel.text = "\(humanYearsTextField.text.toInt()!*7)"
+        if humanYearsTextField.text != ""{
+            var humanYearsDouble = Double((humanYearsTextField.text as NSString).doubleValue)
+            var dogYearsDouble:Double
+            if humanYearsDouble > 0{
+                if humanYearsDouble > 2{
+                    var dogYearsFirstDouble = (humanYearsDouble - 2) * 4
+                    var dogYearsSecondDouble = (2 * 10.5)
+                    dogYearsDouble = dogYearsFirstDouble + dogYearsSecondDouble
+                }
+                else{
+                    dogYearsDouble = humanYearsDouble * 10.5
+                }
+                var dogYearsInt = Int(dogYearsDouble)
+                if dogYearsDouble == Double(dogYearsInt){
+                    dogYearsLabel.text = "\(dogYearsInt)"
+                }
+                else{
+                    dogYearsLabel.text = "\(dogYearsDouble)"
+                }
+                var humanYearsInt = Int(humanYearsDouble)
+                if humanYearsDouble == Double(humanYearsInt){
+                    humanYearsTextField.text = "\(Int(humanYearsDouble))"
+                }
+                else{
+                    humanYearsTextField.text = "\(humanYearsDouble)"
+                }
+            }
+            else{
+                humanYearsTextField.text = "0"
+                dogYearsLabel.text = "0"
+            }
+        }
+        else{
+            dogYearsLabel.text = "0"
+        }
+        //dogYearsLabel.text = "\(humanYearsTextField.text.toInt()!*7)"
         humanYearsTextField.resignFirstResponder()
     }
 
 }
-
